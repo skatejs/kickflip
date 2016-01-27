9bbfad2971c42f329bf12ef5a29001d3d42d6039
Trey Shugart <treshugart@gmail.com>
2016-01-27 15:09:29 +1100

    Add version file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import register from './register';
 import state from './state';
 import vdom from './vdom';
+import version from './version';
 
-export { register, state, vdom };
+export { register, state, vdom, version };
 export default register;
