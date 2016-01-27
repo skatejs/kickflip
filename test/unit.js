26c073deedabcfe8b866808490c12b83a4470528
Trey Shugart <treshugart@gmail.com>
2016-01-27 16:50:14 +1100

    Bump and add initial test.

--- test/unit.js
+++ test/unit.js
@@ -1,3 +1,7 @@
-describe('kickflip', function () {
+import version from '../src/version';
 
+describe('kickflip', function () {
+  it('version', function () {
+    expect(version).to.be.a('string');
+  });
 });
