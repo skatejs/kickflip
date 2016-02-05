61cf7be44606c873a45618936d8e062c64dfefe8
Trey Shugart <treshugart@gmail.com>
2016-02-06 00:09:09 +1100

    Simplify state getting and setting. Setting is simply an Object.assign() call now.

--- src/state.js
+++ src/state.js
@@ -1,21 +1,12 @@
 import assign from 'object-assign';
-import render from 'skatejs/lib/api/render';
 
-function getState (elem) {
+function get (elem) {
   return Object.keys(elem.constructor.properties || {}).reduce(function (prev, curr) {
     prev[curr] = elem[curr];
+    return prev;
   }, {});
 }
 
-function setState(elem, newState) {
-  const ctor = elem.constructor;
-  const shouldUpdate = !ctor.update || ctor.update(elem) !== false;
-  assign(elem, newState);
-  if (shouldUpdate) {
-    render(elem);
-  }
-}
-
 export default function (elem, newState) {
-  return typeof newState === 'undefined' ? getState(elem) : setState(elem, newState);
+  return typeof newState === 'undefined' ? get(elem) : assign(elem, newState);
 }
