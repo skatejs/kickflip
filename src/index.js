b93cdb3e8aa2d9b023bc17246e1d3645d7b656d6
Trey Shugart <treshugart@gmail.com>
2016-02-26 10:27:07 +1100

    Ensure we are pulling in the Skate version we expect to be using, at least until we can specify an semver spec.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import kickflip from './kickflip';
 import link from './link';
-import skate from 'skatejs';
+import skate from '../node_modules/skatejs/lib/index';
 import state from './state';
 import * as vdom from './vdom';
 import version from './version';
