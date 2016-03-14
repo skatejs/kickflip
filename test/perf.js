1282ad3011d636d6a1a0aa5343c22f1b834a7b70
Trey Shugart <treshugart@gmail.com>
2016-03-14 13:27:55 +1100

    Update existing perf tests and add perf test to test re-rendering perf against react.

--- test/perf.js
+++ test/perf.js
@@ -1,4 +1,5 @@
 import bench from 'skatejs-build/bench';
+import { string } from '../src/properties';
 import create, { slot } from '../src/vdom';
 import kickflip from '../src/kickflip';
 
@@ -9,11 +10,17 @@ const { React, ReactDOM } = window;
 // Skate components.
 
 kickflip('x-app', {
-  render () {
-    create('x-list', function () {
-      for (let a = 0; a < 10; a++) {
-        create('x-item', `Item ${a}`);
-      }
+  properties: {
+    title: string({ default: 0 })
+  },
+  render (elem) {
+    create('div', function () {
+      create('h1', elem.title);
+      create('x-list', function () {
+        for (let a = 0; a < 10; a++) {
+          create('x-item', `Item ${a}`);
+        }
+      });
     });
   }
 });
@@ -34,15 +41,21 @@ kickflip('x-item', {
 // React components.
 
 const Xapp = React.createClass({
+  getInitialState () {
+    return { title: 0 };
+  },
   render () {
     const items = [];
     for (let a = 0; a < 10; a++) {
       items.push(a);
     }
-    return React.createElement(Xlist, null,
-      items.map(function (key) {
-        return React.createElement(Xitem, { key }, `Item ${key}`);
-      })
+    return React.createElement('div', null,
+      React.createElement('h1', this.state.title),
+      React.createElement(Xlist, null,
+        items.map(function (key) {
+          return React.createElement(Xitem, { key }, `Item ${key}`);
+        })
+      )
     );
   }
 });
@@ -72,7 +85,7 @@ describe('', function () {
     document.body.removeChild(fixture);
   });
 
-  describe('render', function () {
+  describe('initial render', function () {
     bench('kickflip', function () {
       fixture.innerHTML = '<x-app></x-app>';
       fixture.innerHTML = '';
@@ -82,6 +95,53 @@ describe('', function () {
       ReactDOM.render(React.createElement(Xapp), fixture, function () {
         ReactDOM.unmountComponentAtNode(fixture);
         d.resolve();
+      });
+    });
+  });
+
+  describe('re-render after initial render', function () {
+    describe('', function () {
+      let component;
+
+      beforeEach(function () {
+        fixture.innerHTML = '<x-app></x-app>';
+        component = fixture.firstChild;
+      });
+
+      afterEach(function () {
+        fixture.innerHTML = '';
+      });
+
+      bench('kickflip', {
+        args () {
+          return component;
+        },
+        fn () {
+          this.args().title = `Test ${this.count}`;
+        }
+      });
+    });
+
+    describe('', function () {
+      let component;
+
+      beforeEach(function () {
+        component = ReactDOM.render(React.createElement(Xapp), fixture);
+      });
+
+      afterEach(function () {
+        ReactDOM.unmountComponentAtNode(fixture);
+      });
+
+      bench('react', {
+        args () {
+          return component;
+        },
+        fn () {
+          this.args().setState({
+            title: `Test ${this.count}`
+          });
+        }
       });
     });
   });
